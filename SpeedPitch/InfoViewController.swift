023dd0d5c481ff2429d66e6fc2f9b0b118115b8c
danomatika <danomatika@gmail.com>
2021-07-17 01:26:59 +0200

    load app info from txt file

--- SpeedPitch/InfoViewController.swift
+++ SpeedPitch/InfoViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class InfoViewController: UIViewController {
 
+	@IBOutlet weak var textView: UITextView!
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
+
+		// load rtf into text view
+//		if let path = Bundle.main.url(forResource: "AppInfo", withExtension: "rtf") {
+//			do {
+//				let text: NSAttributedString = try NSAttributedString(url: path, options: [NSAttributedString.DocumentReadingOptionKey.documentType: NSAttributedString.DocumentType.rtf], documentAttributes: nil)
+//				self.textView.attributedText = text
+//			}
+//			catch let error {
+//				print("InfoViewController: could not open AppInfo.rtf: \(error)")
+//			}
+//		}
+		if let path = Bundle.main.url(forResource: "AppInfo", withExtension: "txt") {
+			do {
+				let text: String = try String(contentsOf: path, encoding: .utf8)
+				self.textView.text = text
+			}
+			catch let error {
+				print("InfoViewController: could not open AppInfo.rtf: \(error)")
+			}
+		}
 	}
 
 	@IBAction func done(_ sender: Any) {
